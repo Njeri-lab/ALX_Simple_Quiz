6081288119f5db5d29ddb116e66e34e91b73efb6
Joy Njeri <technjeri25@gmail.com>
2025-06-26 16:55:48 +0300

    .

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 function checkAnswer () {
     const correctAnswer = "4";
-    const userAnswer = document.querySelector('input[name="answer"]:checked').value;
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
     if (userAnswer === correctAnswer) {
         feedback.textContent = "Correct! Well done."
     } else {
